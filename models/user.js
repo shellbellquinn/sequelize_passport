b8c8e9218937ea5e81a0c84ab69df71942d671b8
Michelle Quinn <42622727+shellbellquinn@users.noreply.github.com>
2020-02-11 23:28:12 -0600

    Update pseudo code

--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-// The models define the different columns for a table called. Also note, that in the last line we are exporting Project. This means we will have access to this code through the variable User.
+// The models define the different columns for a table called. Also note, that in the last line we are exporting User. This means we will have access to this code through the variable User.
 
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 var bcrypt = require("bcryptjs");
